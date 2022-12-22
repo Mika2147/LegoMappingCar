0ad54585de588b09a17c99213ce0de9be371a3d8
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-12-22 11:03:31 +0100

    CORRECT RECEIVING

--- morser/receive_morse_data.py
+++ morser/receive_morse_data.py
@@ -1,40 +1,39 @@
 def receive():
     node = {}
     collecting = True
-    id = 0
+    node_id = 0
     b_empty = ColorSensor("B")
     d_next = ColorSensor("D")
+    f_id = ColorSensor("F")
     print("start collecting")
     import time
     morsed_value = NIL
+    counter = 0
     while collecting:
-        node[id] = [id]
-        counter = 0
+        node[node_id] = [node_id]
         if hub.right_button.is_pressed():
             morsed_value += "."
-            print(".")
             time.sleep(1)
         elif hub.left_button.is_pressed():
             morsed_value += "-"
-            print("-")
             time.sleep(1)
         elif not_equal_to(b_empty.get_color(), None):
             morsed_value += " "
-            print(" ")
             time.sleep(1)
         elif not_equal_to(d_next.get_color(), None):
             morsed_value += "/"
-            print("/")
             time.sleep(1)
-            counter += 0.5
-        elif counter == 4: 
-            print("next")
-            node[id].append(de_morse(morsed_value))
-        # TODO get missing sensor and finish following pseudocode
-        # - if slash_counter == 4 then next list and list_counter += 1
-        # - if list_counter == 4 then next node_id
-        # - if missing_sensor.is_pressed() then finished collecting 
+            counter += 1
+        elif not_equal_to(f_id.get_color(), None):
+            node[node_id].append(de_morse(morsed_value))
+            print(de_morse(morsed_value))
+            morsed_value = NIL
         else:
             print(morsed_value)
+            if counter == 12:
+                node[node_id].append(de_morse(morsed_value))
+                node_id += 1
+                print("node id + 1", node_id)
+                counter = 0
             hub.speaker.beep()
     print(morsed_value)
