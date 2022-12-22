c674767927a2bfab90140261c7895c9beaa4345c
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-12-22 09:49:55 +0100

    CORRECT MORSING AND DISPLAYING!!!!!

--- morse_data.py
+++ morse_data.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+
 TESTING = False
 
 if TESTING:
+    from mindstorms import MSHub, Motor, MotorPair, ColorSensor, DistanceSensor, App
+    from mindstorms.control import wait_for_seconds, wait_until, Timer
+    from mindstorms.operator import greater_than, greater_than_or_equal_to, less_than, less_than_or_equal_to, equal_to, not_equal_to
+    import math
     from mindstorms import MSHub, ForceSensor
     hub = MSHub()
 
@@ -24,48 +29,45 @@ nodes = {
 SHORT = "."
 LONG = "-"
 EMPTY = "/"
-BLANK = " " 
-NIL = "" 
+BLANK = " "
+NIL = ""
 WHITESPACE = " // "
 
 
 def generate_morse_codes():
-
-    # ==================== 
+    # ====================
     # ORIGINAL MORSE CODES
-    # ==================== 
+    # ====================
     # morse_codes = {
-    #     "-": LONG + SHORT * 3 + LONG,   # Minus for Negative Values
-    #     ".": (SHORT + LONG) * 3         # Dots for Floats
+    #    "-": LONG + SHORT * 3 + LONG,# Minus for Negative Values
+    #    ".": (SHORT + LONG) * 3        # Dots for Floats
     # }
     # template = [LONG, LONG, LONG, LONG, LONG]
     # morse_codes["0"] = NIL.join(template)
     # for index in range(0, 5):
-    #     template[index] = SHORT
-    #     morse_codes[str(index + 1)] = NIL.join(template)
+    #    template[index] = SHORT
+    #    morse_codes[str(index + 1)] = NIL.join(template)
     # for index in range(0, 4):
-    #     template[index] = LONG
-    #     morse_codes[str(index + 6)] = NIL.join(template)
-    #     template[index] = LONG
+    #    template[index] = LONG
+    #    morse_codes[str(index + 6)] = NIL.join(template)
+    #    template[index] = LONG
 
-    # ==================== 
+    # ====================
     # RAF's SHORTER VARIANT OF MORSE CODES
-    # ==================== 
+    # ====================
     morse_codes = {}
-    morse_codes["0"] = LONG 
+    morse_codes["0"] = LONG
     morse_codes["1"] = SHORT
-    morse_codes["."] = SHORT + SHORT 
-    morse_codes["-"] = SHORT + LONG 
-    morse_codes["2"] = LONG + SHORT 
-    morse_codes["3"] = LONG + LONG 
+    morse_codes["."] = SHORT + SHORT
+    morse_codes["-"] = SHORT + LONG
+    morse_codes["2"] = LONG + SHORT
+    morse_codes["3"] = LONG + LONG
     morse_codes["4"] = SHORT + SHORT + SHORT
     morse_codes["5"] = SHORT + LONG + LONG
     morse_codes["6"] = LONG + SHORT + SHORT
     morse_codes["7"] = LONG + LONG + LONG
     morse_codes["8"] = SHORT + SHORT + SHORT + SHORT
     morse_codes["9"] = SHORT + LONG + LONG + LONG
-    # for key, value in morse_codes.items():
-    #     print(f"{key} -> {value}")
     return morse_codes
 
 
@@ -99,13 +101,13 @@ def de_morse(morse_code):
                 data += str(compare(element)) + BLANK
     shorten = False
     for element in data.strip().split(BLANK):
-        if len(data.strip().split(BLANK)) == 2: 
-            res.append(int(element) if float(element).is_integer() else float(element))
+        if len(data.strip().split(BLANK)) == 2:
+            res.append((element))
             shorten = True
-        else: 
-            res.append(int(element) if float(element).is_integer() else float(element))
-            shorten = False 
-    if shorten: 
+        else:
+            res.append((element))
+            shorten = False
+    if shorten:
         res.append(-1)
         res.append(-1)
     return res
@@ -113,42 +115,43 @@ def de_morse(morse_code):
 
 def display_morse(morse_code):
     character = []
-    for character in morse_code:
-        if character == SHORT or character == LONG or character == EMPTY:
+    import time
+    for character in morse_code.strip():
+        if character == BLANK: 
+            hub.light_matrix.write("o")
+        else:
             hub.light_matrix.write(character)
-        hub.left_button.wait_until_pressed()
+        time.sleep(2)
+        hub.light_matrix.write("")
+        time.sleep(2)
+    hub.speaker.beep()
 
 
-def display(character):
-    hub.light_matrix.write(character)
-    hub.left_button.wait_until_pressed()
-
-
-def receive():
-    """
-    TODO the logic is more or less done but not tested
-    """
-    node = {}
-    collecting = True
-    id = 0
-    button_force = ForceSensor("E")
-    while collecting:
-        node[id] = [id]
-        morsed_value = NIL
-        counter = 0
-        if hub.left_button.is_pressed():
-            morsed_value += SHORT
-        if hub.right_button.is_pressed():
-            morsed_value += LONG
-        if hub.left_button.is_pressed() and hub.left_button.is_pressed():
-            counter += 1
-            morsed_value += EMPTY + EMPTY
-        if counter == 4:
-            node[id].append(de_morse(morsed_value))
-        if button_force.is_pressed():
-            id += 1
-        if button_force.is_pressed() and hub.left_button.is_pressed():
-            collecting = False
+# def receive():
+#     """
+#     TODO the logic is more or less done but not tested
+#     """
+#     node = {}
+#     collecting = True
+#     id = 0
+#     button_force = ForceSensor("E")
+#     while collecting:
+#         node[id] = [id]
+#         morsed_value = NIL
+#         counter = 0
+#         if hub.left_button.is_pressed():
+#             morsed_value += SHORT
+#         if hub.right_button.is_pressed():
+#             morsed_value += LONG
+#         if hub.left_button.is_pressed() and hub.left_button.is_pressed():
+#             counter += 1
+#             morsed_value += EMPTY + EMPTY
+#         if counter == 4:
+#             node[id].append(de_morse(morsed_value))
+#         if button_force.is_pressed():
+#             id += 1
+#         if button_force.is_pressed() and hub.left_button.is_pressed():
+#             collecting = False
 
 
 def main():
@@ -162,22 +165,20 @@ def main():
             if isinstance(element, list):
                 if element[2] == -1:
                     morsed_word = morse(element[0]) + WHITESPACE + morse(element[1]) + WHITESPACE * 2
-                else: 
+                else:
                     for value in element:
                         for splitted in str(value).strip().split(BLANK):
-                            splitted = float(splitted)
-                            value = splitted if not splitted.is_integer() else int(splitted)
+                            value = splitted
                         morsed_value = morse(value)
-                        morsed_word += morsed_value + WHITESPACE 
-                # display_morse(morsed_word)
+                        morsed_word += morsed_value + WHITESPACE
                 # print(f"{element} ==> {morsed_word} ==> {de_morse(morsed_word)}")
                 reconstructed[node].append(de_morse(morsed_word))
-            # print(morsed_word)
+            print(morsed_word)
+            display_morse(morsed_word)
             morsed_word = NIL
-        # display_morse("N")
-    print("ORIGINAL DATA\n", nodes)
-    print("MORSED AND PREPARED DATA\n", reconstructed)
+        display_morse("N")
+    # print("ORIGINAL DATA\n", nodes)
+    # print("MORSED AND PREPARED DATA\n", reconstructed)
 
 
-if __name__ == "__main__":
-    main()
+main()
