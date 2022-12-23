7837e798c6e55326f09913e29f2f6a57bf9d3b12
Raphaele Salvatore Licciardo <raphaele.salvatore@outlook.de>
2022-12-23 09:39:36 +0100

    Receiver sees Input, ints and no floats, shorter morse data, klick so see next morseitem when sending.

--- morser/morse_data.py
+++ morser/morse_data.py
@@ -11,17 +11,11 @@ try:
     b_empty = ColorSensor("B")
     d_next = ColorSensor("D")
     f_id = ColorSensor("F")
-except Exception: 
+except Exception:
     print("Not every Sensor is correct pluged in!")
     import sys
     sys.exit(0)
 
-# Knoten
-# (id, Kante 1, Kante 2, Kante 3, Kante 4)
-# Kante
-# (Richtung x, Richtung y, Zielknoten, Entfernung)
-# -1 means unknown
-# -2 means no destiantion
 nodes = {
     0: [0, [1, 0, 1, 48.5], [0, 1, -1, -1], [-1, 0, -1, -1], [0, -1, -1, -1]],
     1: [1, [1, 0, -1, -1], [0, 1, 2, 112.0], [-1, 0, 0, 48.5], [0, -1, -1, -1]],
@@ -36,43 +30,31 @@ LONG = "-"
 EMPTY = "/"
 BLANK = " "
 NIL = ""
-WHITESPACE = " // "
-
+# WHITESPACE = "//"
+WHITESPACE = EMPTY
 
 def generate_morse_codes():
-    # ====================
-    # ORIGINAL MORSE CODES
-    # ====================
-    # morse_codes = {
-    #    "-": LONG + SHORT * 3 + LONG,# Minus for Negative Values
-    #    ".": (SHORT + LONG) * 3        # Dots for Floats
-    # }
-    # template = [LONG, LONG, LONG, LONG, LONG]
-    # morse_codes["0"] = NIL.join(template)
-    # for index in range(0, 5):
-    #    template[index] = SHORT
-    #    morse_codes[str(index + 1)] = NIL.join(template)
-    # for index in range(0, 4):
-    #    template[index] = LONG
-    #    morse_codes[str(index + 6)] = NIL.join(template)
-    #    template[index] = LONG
-
-    # ====================
-    # RAF's SHORTER VARIANT OF MORSE CODES
-    # ====================
     morse_codes = {}
     morse_codes["0"] = LONG
     morse_codes["1"] = SHORT
-    morse_codes["."] = SHORT + SHORT
-    morse_codes["-"] = SHORT + LONG
-    morse_codes["2"] = LONG + SHORT
-    morse_codes["3"] = LONG + LONG
-    morse_codes["4"] = SHORT + SHORT + SHORT
-    morse_codes["5"] = SHORT + LONG + LONG
-    morse_codes["6"] = LONG + SHORT + SHORT
-    morse_codes["7"] = LONG + LONG + LONG
-    morse_codes["8"] = SHORT + SHORT + SHORT + SHORT
-    morse_codes["9"] = SHORT + LONG + LONG + LONG
+
+    morse_codes["-"] = SHORT + SHORT
+    morse_codes["2"] = SHORT + LONG
+
+    morse_codes["3"] = SHORT + SHORT + SHORT
+    morse_codes["4"] = SHORT + SHORT + LONG
+
+    morse_codes["5"] = SHORT + LONG + SHORT
+    morse_codes["6"] = SHORT + LONG + LONG
+
+    morse_codes["5"] = LONG + SHORT + SHORT
+    morse_codes["6"] = LONG + SHORT + LONG
+
+    morse_codes["7"] = LONG + LONG + SHORT
+    morse_codes["8"] = LONG + LONG + LONG
+
+    morse_codes["9"] = SHORT + SHORT + SHORT + SHORT
+
     return morse_codes
 
 
@@ -95,7 +77,7 @@ def de_morse(morse_code):
 
     data = NIL
     res = []
-    for element in morse_code.split(EMPTY + EMPTY):
+    for element in morse_code.split(EMPTY):
         element = element.strip()
         if BLANK in element:
             for word in element.split(BLANK):
@@ -119,14 +101,20 @@ def de_morse(morse_code):
     return res
 
 
-def display_morse(morse_code):
+def display_morse(morse_code, wait=True):
     character = []
     import time
     for character in morse_code.strip():
         if character == BLANK:
             hub.light_matrix.write("o")
+        elif character == SHORT:
+            hub.light_matrix.write("S")
+        elif character == LONG:
+            hub.light_matrix.write("L")
         else:
             hub.light_matrix.write(character)
+        if wait:
+            hub.right_button.wait_until_pressed()
         time.sleep(1)
         hub.light_matrix.write("")
         time.sleep(1)
@@ -142,15 +130,19 @@ def receive():
         if hub.right_button.is_pressed():
             morsed_value += SHORT
             time.sleep(1)
+            display_morse("S", False)
         elif hub.left_button.is_pressed():
             morsed_value += LONG
             time.sleep(1)
+            display_morse("L", False)
         elif not_equal_to(b_empty.get_color(), None):
             morsed_value += BLANK
             time.sleep(1)
+            display_morse("_", False)
         elif not_equal_to(d_next.get_color(), None):
             morsed_value += WHITESPACE
             time.sleep(1)
+            display_morse("/", False)
         elif not_equal_to(f_id.get_color(), None):
             if len(morsed_value) > 1:
                 tmp = de_morse(morsed_value)
@@ -160,24 +152,12 @@ def receive():
         else:
             print(morsed_value, node)
             hub.speaker.beep()
-            if len(node[node_id]) == 5: 
+            if len(node[node_id]) == 5:
                 node_id += 1
 
-    # TODO how to stop collecting
-    # idea 1
-    # - every node_id += 1 iteration we save the data on the hub in a file
-    # - we restart the hub on another programm space this is reading the data of file 
-    # idea 2 
-    # - if there are 10 seconds long, no input, the data is going to be used
- 
-    # TODO document everything 
 
-def send(debug=False):
-    if debug: reconstructed = {}
-    for node, data in nodes.items():
-        if debug:
-            print("Der Knoten", node, "hat folgende Elemente")
-            reconstructed[node] = [node]
+def send():
+    for _, data in nodes.items():
         morsed_word = NIL
         for element in data:
             morsed_value = NIL
@@ -186,19 +166,14 @@ def send(debug=False):
                     morsed_word = morse(element[0]) + WHITESPACE + morse(element[1]) + WHITESPACE * 2
                 else:
                     for value in element:
-                        for splitted in str(value).strip().split(BLANK):
+                        for splitted in str(int(value)).strip().split(BLANK):
                             value = splitted
                         morsed_value = morse(value)
                         morsed_word += morsed_value + WHITESPACE
-                if debug: reconstructed[node].append(de_morse(morsed_word))
-            if debug and type(element) is list: print(element, " => ", morsed_word, " => ", de_morse(morsed_word))
-            if not debug: 
-                print(morsed_word)
-                display_morse(morsed_word)
+            print(morsed_word)
+            display_morse(morsed_word)
             morsed_word = NIL
-        if not debug: display_morse("N")
-    if debug: print("ORIGINAL DATA\n", nodes)
-    if debug: print("MORSED AND PREPARED DATA\n", reconstructed)
+        display_morse("N")
 
-send()
-# receive()
+# send()
+receive()
